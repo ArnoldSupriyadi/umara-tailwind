5598e8044781bcbec922bf76b2d9231a9ed4f2b9
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2025-08-24 23:32:54 +0700

    create page about contact alos about

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: '#3D717E',
+          200: '#10475E',
+          300: '#0B2839'
+        },
+        amber : {
+          100: '#D68631',
+          200: '#964405',
+          300: '#5A3211'
+        }
+      },
+      fontFamily: {
+        'nunito': ['Nunito Sans', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 
